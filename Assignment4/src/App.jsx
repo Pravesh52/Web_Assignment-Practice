6979612883910a4b5044f6a631c2d7f2a9c97adc
PRAVESH TIWARI <pravesht252@gmail.com>
2026-05-27 16:06:25 +0530

    Assignment

---
+++ Assignment4/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [input,setinput]=useState([]);
+  const [todo,settodo]=useState("");
+
+  // Add task
+  const addtask=()=>{
+    
+  }
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
